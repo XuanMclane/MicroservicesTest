211edfd9033243b03879979673b98a569e852cda
XuanMclane <xuanmclane@gmail.com>
2020-08-08 20:39:42 -0400

    category service

---
+++ CatagoryService/Database/CategoryServiceContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatagoryService.Database
+{
+    public class CategoryServiceContext : DbContext
+    {
+        public CategoryServiceContext(DbContextOptions<CategoryServiceContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
